9d25e24a0658cf24ee5be91fc8f86400680609af
QGGB07 <qgb23300@email.vccs.edu>
2025-07-11 00:03:54 +0000

    Initial Code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+    // Pattern A
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
+    // Pattern B
+    for (int i = 10; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
